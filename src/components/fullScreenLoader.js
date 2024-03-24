971e17b584c679ba80d02f583d059c6cb8038fa9
mejedalkoutaini <mejed@pintofinancial.com>
2024-03-24 05:23:36 +0300

    Adjusted fullScreenLoader

--- src/components/fullScreenLoader.js
+++ src/components/fullScreenLoader.js
@@ -1,5 +1,5 @@
 const FullScreenLoader = () => (
-  <div className="fixed w-full h-screen top-0 left-0 flex items-center justify-center">
+  <div className="fixed w-full h-screen top-[67px] left-0 flex items-center justify-center">
     <span className="loading loading-infinity w-28 bg-teal-600"></span>
     <span className="fixed bottom-4 left-1/2 -translate-x-1/2 text-primary text-xl font-medium">RoomAI</span>
   </div>
