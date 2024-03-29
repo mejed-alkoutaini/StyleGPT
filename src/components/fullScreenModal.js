507e14004b65a7c407db8b8b1434b9cc58f0eea2
mejedalkoutaini <mejed@pintofinancial.com>
2024-03-29 05:02:54 +0300

    Fixed full screen blocker

--- src/components/fullScreenModal.js
+++ src/components/fullScreenModal.js
@@ -31,60 +31,75 @@ const FullScreenModal = (props) => {
   };
 
   useEffect(() => {
+    if (!active) return;
     handleImageLoad();
   }, [imageRef, imageAfter, imageBefore, comparison, windowSize]);
   return (
-    <div
-      className={`fixed top-0 left-0 w-full h-screen flex items-center justify-center transition-all ${
-        active ? "opacity-100 visible" : "opacity-0 invisible"
-      }`}
-    >
-      <div className="flex items-center gap-6 absolute top-0 right-0 py-2 px-6 z-50 bg-teal-600 rounded-bl-lg">
-        {!disablePublishing && (
-          <div className="tooltip tooltip-bottom cursor-pointer" data-tip={isPublished ? "UnPublish" : "Publish"}>
-            <CloudArrowUpIcon width={30} height={30} color="white" onClick={onPublish} />
+    <>
+      {active && (
+        <div
+          className={`fixed top-0 left-0 w-full h-screen flex items-center justify-center transition-all z-50 ${
+            active ? "opacity-100 visible" : "opacity-0 invisible"
+          }`}
+        >
+          <div className="flex items-center gap-6 absolute top-0 right-0 py-2 px-6 z-50 bg-teal-600 rounded-bl-lg">
+            {!disablePublishing && (
+              <div className="tooltip tooltip-bottom cursor-pointer" data-tip={isPublished ? "UnPublish" : "Publish"}>
+                <CloudArrowUpIcon width={30} height={30} color="white" onClick={onPublish} />
+              </div>
+            )}
+
+            <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Side by Side Comparison">
+              <ArrowsRightLeftIcon width={24} height={24} color="white" onClick={() => setComparison(!comparison)} />
+            </div>
+
+            {!disableDownloading && (
+              <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Download">
+                <ArrowDownCircleIcon width={30} height={30} color="white" onClick={onDownload} />
+              </div>
+            )}
+
+            <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Close">
+              <XCircleIcon width={30} height={30} color="white" onClick={closeModalHandler} />
+            </div>
           </div>
-        )}
 
-        <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Side by Side Comparison">
-          <ArrowsRightLeftIcon width={24} height={24} color="white" onClick={() => setComparison(!comparison)} />
-        </div>
-
-        {!disableDownloading && (
-          <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Download">
-            <ArrowDownCircleIcon width={30} height={30} color="white" onClick={onDownload} />
+          <div
+            className={`${!comparison ? "relative opacity-100 visible" : "absolute opacity-0 invisible"} select-none`}
+          >
+            <img src={imageAfter} className="fullScreenImage" ref={imageRef} />
           </div>
-        )}
 
-        <div className="tooltip tooltip-bottom cursor-pointer" data-tip="Close">
-          <XCircleIcon width={30} height={30} color="white" onClick={closeModalHandler} />
-        </div>
-      </div>
-
-      <div className={`${!comparison ? "relative opacity-100 visible" : "absolute opacity-0 invisible"} select-none`}>
-        <img src={imageAfter} className="fullScreenImage" ref={imageRef} />
-      </div>
-
-      {comparison && (
-        <ReactCompareSlider
-          className={`bg-rd select-none w-[${dimensions.width}px] h-[${dimensions.height}px]`}
-          style={{ overflow: "visible", width: dimensions.width, height: dimensions.height }}
-          itemOne={
-            <ReactCompareSliderImage
-              src={imageBefore}
-              key={imageBefore}
-              alt="Image one"
-              className={`fullScreenImage `}
+          {comparison && (
+            <ReactCompareSlider
+              className={`bg-rd select-none w-[${dimensions.width}px] h-[${dimensions.height}px]`}
+              style={{ overflow: "visible", width: dimensions.width, height: dimensions.height }}
+              itemOne={
+                <ReactCompareSliderImage
+                  src={imageBefore}
+                  key={imageBefore}
+                  alt="Image one"
+                  className={`fullScreenImage `}
+                />
+              }
+              itemTwo={
+                <ReactCompareSliderImage
+                  src={imageAfter}
+                  key={imageAfter}
+                  alt="Image two"
+                  className="fullScreenImage"
+                />
+              }
             />
-          }
-          itemTwo={
-            <ReactCompareSliderImage src={imageAfter} key={imageAfter} alt="Image two" className="fullScreenImage" />
-          }
-        />
-      )}
+          )}
 
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80 z-[-1]" onClick={closeModalHandler}></div>
-    </div>
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-black opacity-80 z-[-1]"
+            onClick={closeModalHandler}
+          ></div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,15 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { initializePaddle } from "@paddle/paddle-js";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { toast } from "react-hot-toast";
+import { useUserData } from "@/contexts/userDataContext";
+import { useAuth } from "@/contexts/authContext";
+import { getUser } from "@/utils/api";
 
 const paddleToken = process.env.NEXT_PUBLIC_PADDLE_KEY;
 
 export default function Checkout() {
+  const { currentUser } = useAuth();
+  const { setUserData } = useUserData();
   const searchParams = useSearchParams();
   const transactionId = searchParams.get("txn");
   const router = useRouter();
+  const timeoutRef = useRef();
 
   useEffect(() => {
     if (!transactionId) {
@@ -20,10 +26,19 @@ export default function Checkout() {
     initializePaddle({
       environment: "sandbox",
       token: paddleToken,
-      eventCallback: function (data) {
+      eventCallback: async function (data) {
         if (data.name == "checkout.completed") {
-          toast.success("Your Credits Have Been Added 🎉");
-          router.push("/explore");
+          // Clear any existing timeout to ensure we don't have duplicate calls
+          clearTimeout(timeoutRef.current);
+
+          // Set a new timeout
+          timeoutRef.current = setTimeout(async () => {
+            // Assuming getUser is an async function that fetches user data
+            const userData = await getUser(currentUser?.uid);
+            setUserData(userData);
+            router.push("/explore");
+            toast.success("Your Credits Have Been Added 🎉");
+          }, 2000); // 2000 milliseconds = 2 seconds
         }
       },
     }).then((paddleInstance) => {
